9abbd2c6c20f87b67d9261e5adf63b225cd09632
Kassim Ouattara <ouat.kassim@gmail.com>
2017-10-10 14:02:10 -0400

    Creating CI blog, adding controllers and views home, about, creating model post and database, adding the view page for posts

---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+        <link rel="stylesheet" href="http://localhost:8888/ciblog/assets/css/styles.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-hearder">
+                    <a href="/" class="navbar-brand">ciBlog</a>
+                </div>
+                <div id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>about">About</a></li>
+                        <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    <div class="container">
+    
