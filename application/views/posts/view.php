cd9ca60b5ad47fb6de4184d9682bc9237b468079
Kassim Ouattara <ouat.kassim@gmail.com>
2017-10-17 14:24:02 -0400

    Adding CRUD on posts

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -4,3 +4,9 @@
 <br>
     <?php echo $post['body']; ?>
 </div>
+
+<hr>
+<a href="edit/<?php echo $post['slug']; ?>" class='btn btn-default pull-left'>Edit</a>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+    <input type="submit" value='delete' class='btn btn-danger'>
+</form>
